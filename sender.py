28e50a497f99ea8064782c0783217d1b7dabf00f
Artem Grunichev <gluk@umbrosia.ru>
2019-02-21 13:40:40 +0000

    fixed sending downlink messages, now every message sent

--- sender.py
+++ sender.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 import paho.mqtt.client as mqtt
+import time
+
+index = 0
+fname = '/home/pi/lora_gateway/downlink/downlink-post.txt'
+fname = '/home/pi/lora_gateway/downlink/downlink.txt'
 
 def post(data):
-	with open('/home/pi/lora_gateway/downlink/downlink-post.txt', 'w') as outfile:
+	with open(fname, 'w') as outfile:
 		outfile.write(data)
+	print (data)
 
 def on_connect(client, userdata, flags, rc):
 	#print('Connected with result code {}'.format(rc))
 	client.subscribe("#")
 
 def on_message(client, userdata, msg):
+	global index
 	dest = ''.join([i for i in msg.topic if i in '0123456789'])
-	message = str(msg.payload.decode())
+	message = str(msg.payload.decode()) + str(index)
 	towrite = '{'+'"status":"send_request","dst":{},"data":"{}"'.format(dest, message)+'}\n'
 	post(towrite)
-	#print("{} {}".format(msg.topic, msg.payload))
+	index += 1
 
 client = mqtt.Client()
 client.on_connect = on_connect
