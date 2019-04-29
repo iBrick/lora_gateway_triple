aca99406281016d4fe3fda9c7b821390663ba0b8
Artem Grunichev <gluk@umbrosia.ru>
2019-04-29 18:50:24 +0300

    changed SPI device number to 0.0

--- SPI.h
+++ SPI.h
@@ -23,64 +23,71 @@ SOFTWARE.
 *
 *  Created on: Sep 4, 2017
 *  Author: Vinay Divakar
-*/ 
+*/
 
 #ifndef SPI_H_
 #define SPI_H_
 
-#include<stdint.h>
+#include <stdint.h>
 
 /* Generic Definitions */
-#define SPIDEV_BYTES_NUM                 8
-#define SPIDEV_DATA_BITS_NUM             8
-#define SPIDEV_DELAY_US	                 0
-#define SPI_SS_HIGH                      1
-#define SPI_SS_LOW                       0
-#define SPI_ONE_BYTE                     1
+#define SPIDEV_BYTES_NUM 8
+#define SPIDEV_DATA_BITS_NUM 8
+#define SPIDEV_DELAY_US 0
+#define SPI_SS_HIGH 1
+#define SPI_SS_LOW 0
+#define SPI_ONE_BYTE 1
 
 /* No. of bytes per transaction */
-#define NO_OF_BYTES                      2
+#define NO_OF_BYTES 2
 
 /*Definitions specific to spidev1.0 */
-#define SPIDEV1_PATH                     "/dev/spidev1.0"
-#define SPIDEV1_BUS_SPEED_HZ             100000
-
+#define SPIDEV1_PATH "/dev/spidev0.0" // RNH: changed to 0.0 with kernel update
+#define SPIDEV1_BUS_SPEED_HZ 100000
 
 /* Enum SPI Modes*/
-typedef enum{
-	SPI_MODE0 = 0,
-	SPI_MODE1 = 1,
-	SPI_MODE2 = 2,
-	SPI_MODE3 = 3
-}SPI_MODE;
+typedef enum {
+  SPI_MODE0 = 0,
+  SPI_MODE1 = 1,
+  SPI_MODE2 = 2,
+  SPI_MODE3 = 3
+} SPI_MODE;
 
 /*SPI device configuration structure*/
-typedef struct{
-	char* spi_dev_path;
-	int fd_spi;
-	unsigned long spi_bytes_num;
-	unsigned long spi_bus_speedHZ;
-	unsigned char ss_change;
-	unsigned short spi_delay_us;
-	unsigned char spi_data_bits_No;
-	unsigned char spi_mode;
-}SPI_DeviceT, *SPI_DevicePtr;
+typedef struct {
+  char *spi_dev_path;
+  int fd_spi;
+  unsigned long spi_bytes_num;
+  unsigned long spi_bus_speedHZ;
+  unsigned char ss_change;
+  unsigned short spi_delay_us;
+  unsigned char spi_data_bits_No;
+  unsigned char spi_mode;
+} SPI_DeviceT, *SPI_DevicePtr;
 
 /* SPI API's*/
 extern int Open_device(char *spi_dev_path, int *fd);
 extern int Set_SPI_mode(int fd, unsigned char spi_mode);
 extern int Set_SPI_bits(int fd, unsigned char bits_per_word);
 extern int Set_SPI_speed(int fd, unsigned long bus_speed_HZ);
-extern void SPI_Config_init(unsigned long spi_bytes_no, unsigned long spi_bus_speed,
-                unsigned char chip_select, unsigned short spi_delay,
-                unsigned char spi_bits_No, unsigned char mode_spi, SPI_DevicePtr SPI_devicePtr);
+extern void SPI_Config_init(unsigned long spi_bytes_no,
+                            unsigned long spi_bus_speed,
+                            unsigned char chip_select,
+                            unsigned short spi_delay,
+                            unsigned char spi_bits_No,
+                            unsigned char mode_spi,
+                            SPI_DevicePtr SPI_devicePtr);
 
 /* API's to initialize and use spidev1.0 - Configure the Bus as you like*/
-extern int SPI_DEV1_init(unsigned long spi_bytes_no, unsigned long spi_bus_speed,
-                                           unsigned char chip_select, unsigned short spi_delay,
-                                           unsigned char spi_bits_No, unsigned char mode_spi);
-extern int SPIDEV1_transfer(unsigned char *send, unsigned char *receive,
-                unsigned char bytes_num);
+extern int SPI_DEV1_init(unsigned long spi_bytes_no,
+                         unsigned long spi_bus_speed,
+                         unsigned char chip_select,
+                         unsigned short spi_delay,
+                         unsigned char spi_bits_No,
+                         unsigned char mode_spi);
+extern int SPIDEV1_transfer(unsigned char *send,
+                            unsigned char *receive,
+                            unsigned char bytes_num);
 extern unsigned char SPIDEV1_single_transfer(unsigned char data_byte);
 
 #endif /* SPI_H_ */
