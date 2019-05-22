03fa667a58127562f721a9286c3bc94c64576c81
Artem Grunichev <gluk@umbrosia.ru>
2019-05-22 18:24:43 +0300

    fixed lvalue error

--- lora_gateway.cpp
+++ lora_gateway.cpp
@@ -374,11 +374,11 @@ int xtoi(const char *hexstring);
 //#else
 // uint8_t loraChannelIndex = 7;
 // RNH: channel moved to CH_08_868 because of noise
-// uint8_t loraChannelIndex = 4;
 // RNH: channel set in makefile, if not, default to 4 (08)
-#  ifndef loraChannelIndex
-#    define loraChannelIndex (4)
+#  ifndef LCI
+#    define LCI (4)
 #  endif
+uint8_t loraChannelIndex = LCI;
 //#endif
 uint32_t loraChannelArray[MAX_NB_CHANNEL] = {
     CH_04_868, CH_05_868, CH_06_868, CH_07_868, CH_08_868,
