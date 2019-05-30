d2c57b39a75de6b7fcc973a978ded6b3877bf1a4
Artem Grunichev <gluk@umbrosia.ru>
2019-05-30 13:40:37 +0300

    simpler output handling for downlinker

--- scripts/bb_triple_restart.sh
+++ scripts/bb_triple_restart.sh
@@ -5,8 +5,7 @@ scripts/config_board.sh
 bash scripts/bb_triple_stop.sh &> /dev/null
 
 ./lora_gateway_bb_module0 | python post_processing_gw.py &> log/module0_post_processing &
-#./lora_gateway_bb_module1 --raw | python post_processing_gw.py &> log/module1_post_processing &
-./downlinker &> log/downlinker &
+./downlinker > log/downlinker &
 ./lora_gateway_bb_module2 | python post_processing_gw.py &> log/module2_post_processing &
 
 echo 'modules started, outputs are in logs folder'
