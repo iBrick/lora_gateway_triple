a8ced40b7e500549a387155079a6d863e5678151
Artem Grunichev <gluk@umbrosia.ru>
2019-06-03 18:47:16 +0300

    fixed .service files path

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ else
 		echo "Building executables"
 		make
 		echo "Setting up sustemd services (to be run after mqttclient)"
-		sudo ln -s /home/pi/downlinker.service /etc/systemd/system/
-		sudo ln -s /home/pi/uplinker-plain.service /etc/systemd/system/
-		sudo ln -s /home/pi/uplinker-alarm.service /etc/systemd/system/
+		sudo ln -s /home/pi/lora_gateway/downlinker.service /etc/systemd/system/
+		sudo ln -s /home/pi/lora_gateway/uplinker-plain.service /etc/systemd/system/
+		sudo ln -s /home/pi/lora_gateway/uplinker-alarm.service /etc/systemd/system/
 		echo "TODO: time setup, write time to RTC."
 		echo "Setup finished. You may restart now."
 	)
