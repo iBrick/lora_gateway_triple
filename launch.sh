aebcf38f7c38b6193d3cd6d3c801963c9ab53590
Artem Grunichev <gluk@umbrosia.ru>
2019-06-03 17:38:01 +0300

    fixed typos

--- launch.sh
+++ launch.sh
@@ -6,9 +6,9 @@ killall uplinker
 killall downlinker
 (
     cd /home/pi/lora_gateway
-    ./uplinker /dev/spidev0.2 4 1 1 14 | python post_processing_gw.py &> log/module0_post_processing &
-    ./downlinker > log/downlinker &
-    ./uplinker /dev/spidev0.2 4 1 1 14 | python post_processing_gw.py &> log/module0_post_processing &
+    ./uplinker /dev/spidev0.0 4 1 1 14 | python post_processing_gw.py &> log/uplinker_plain &
+    ./downlinker /dev/spidev0.1 6 1 1 14 > log/downlinker &
+    ./uplinker /dev/spidev0.2 8 1 1 14 | python post_processing_gw.py &> log/uplinker_alarm &
 
     echo 'modules started, outputs are in logs folder'
     echo 'killing process would also kill all pythons'
