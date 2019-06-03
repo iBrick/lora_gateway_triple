eec8662d1ee95de547c94f7728a6a5c7f96aad62
Artem Grunichev <gluk@umbrosia.ru>
2019-06-03 15:49:32 +0300

    typo

--- SX1272.cpp
+++ SX1272.cpp
@@ -592,7 +592,7 @@ uint8_t SX1272::setLORA() {
         digitalWriteSafely(SX1272_RST,LOW);
     }
     */
-    printf("%s\r\n", st0);
+    printf("0x%X\r\n", st0);
 
   } while (st0 != LORA_STANDBY_MODE); // LoRa standby mode
 
