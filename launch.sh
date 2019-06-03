f31a358c5b089a61c8f7c1e8426deeae67507ec5
Artem Grunichev <gluk@umbrosia.ru>
2019-06-03 17:36:18 +0300

    launch.sh actualized

--- launch.sh
+++ launch.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-sudo ./scripts/bb_triple_restart.sh
+# run with sudo or from root
+#sudo ./scripts/bb_triple_restart.sh
+
+killall uplinker 
+killall downlinker
+(
+    cd /home/pi/lora_gateway
+    ./uplinker /dev/spidev0.2 4 1 1 14 | python post_processing_gw.py &> log/module0_post_processing &
+    ./downlinker > log/downlinker &
+    ./uplinker /dev/spidev0.2 4 1 1 14 | python post_processing_gw.py &> log/module0_post_processing &
+
+    echo 'modules started, outputs are in logs folder'
+    echo 'killing process would also kill all pythons'
+    echo 'process will be killed on Return key press.'
+    read
+)
+
+#setsid -c sleep infinity
+
+#kill $(jobs -p)
