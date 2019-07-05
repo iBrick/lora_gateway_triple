62ed44c976444534956cb1f2036cba29866cd6c0
Artem Grunichev <gluk@umbrosia.ru>
2019-07-05 19:04:44 +0300

    bunch of typos fixed

--- decipher.py
+++ decipher.py
@@ -8,13 +8,13 @@ nwskey = appskey
 if len( sys.argv ) < 2 or 'h' in sys.argv[ 1 ]:
 	print( 'Usage (all data in hex): {} [payload] [appskey] [nwskey]'.format( sys.argv[ 0 ] ) )
 else:
-	payload = list( bytearray.fromhex( sys.argv[ 1 ] )
+	payload = list( bytearray.fromhex( sys.argv[ 1 ] ) )
 
 	if len(sys.argv) > 2:
-		appskey = list( bytearray.fromhex( sys.argv[ 2 ] )
+		appskey = list( bytearray.fromhex( sys.argv[ 2 ] ) )
 
 	if len(sys.argv) > 3:
-		nwskey = list( bytearray.fromhex( sys.argv[ 3 ] )
+		nwskey = list( bytearray.fromhex( sys.argv[ 3 ] ) )
 
 	lorawan = LoRaWAN.new(nwskey, appskey)
 	lorawan.read(payload)
