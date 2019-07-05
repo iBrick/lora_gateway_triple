7cb201b48ce87cd4c01b34044b2741c9a35760ab
Artem Grunichev <gluk@umbrosia.ru>
2019-07-05 21:21:59 +0300

    nkskey -> nwkskey

--- decipher.py
+++ decipher.py
@@ -3,7 +3,7 @@ import sys
 import LoRaWAN
 
 appskey = [0]*16
-nwskey = appskey
+nwkskey = appskey
 
 if len( sys.argv ) < 2 or 'h' in sys.argv[ 1 ]:
 	print( 'Usage (all data in hex): {} [payload] [appskey] [nwskey]'.format( sys.argv[ 0 ] ) )
@@ -14,8 +14,8 @@ else:
 		appskey = list( bytearray.fromhex( sys.argv[ 2 ] ) )
 
 	if len(sys.argv) > 3:
-		nwskey = list( bytearray.fromhex( sys.argv[ 3 ] ) )
+		nwkskey = list( bytearray.fromhex( sys.argv[ 3 ] ) )
 
-	lorawan = LoRaWAN.new(nwskey, appskey)
+	lorawan = LoRaWAN.new(nwkskey, appskey)
 	lorawan.read(payload)
 	print( ''.join('{:02X}'.format(i) for i in lorawan.get_payload()))
